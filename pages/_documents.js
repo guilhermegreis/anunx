1b457f4e9edd2be4d318232e97fb791ccb049c53
Guilherme <guiglreis@gmail.com>
2024-06-04 23:26:33 -0300

    Adicionando Header, Dashboard de user e estilizações de botões

--- pages/_documents.js
+++ pages/_documents.js
@@ -9,7 +9,10 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="theme-color" content={theme.palette.primary.main} />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css? family=Roboto:300,400,500,700&display=swap"
+                    />
                 </Head>
                 <body>
                     <Main />
