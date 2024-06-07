3fab02a7e212507b09c1ac37495555021aab4931
Guilherme <guiglreis@gmail.com>
2024-06-06 23:12:09 -0300

    adicionando upload de imagens com dropzone

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,9 +1,24 @@
-import { Box, Button, Container, Select, TextField, Typography } from "@material-ui/core"
+import {
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Select,
+    TextField,
+    Typography 
+} from "@material-ui/core"
+    
 import { makeStyles } from "@material-ui/core/styles"
+import { useDropzone } from "react-dropzone"
 
 import TemplateDefault from "../../src/templates/Default"
+import { DeleteForever } from "@material-ui/icons"
+import { useState } from "react"
 
 const useStyles = makeStyles((theme) =>( {
+    mask: {},
+    mainImage: {},
+
     container: {
         padding: theme.spacing(8, 0, 6)
     },
@@ -18,11 +33,75 @@ const useStyles = makeStyles((theme) =>( {
     buttonEnv: {
         backgroundColor: theme.palette.background.black,
         padding: theme.spacing(0),
+    },
+    thumbsContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginTop: 15,
+    },
+    dropzone: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: '10px',
+        width: 200,
+        height: 150,
+        margin: '0 15px 15px 0',
+        backgroundColor: theme.palette.background.default,
+        border: '2px dashed black'
+    },
+    thumb: {
+        position: 'relative',
+        width: 200,
+        height: 150,
+        backgroundSize: 'cover',
+        margin: '0 15px 15px 0',
+        backgroundPosition: 'center center',
+
+        '& $mainImage': {
+            backgroundColor: 'blue',
+            padding: '6px 10px',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+        },
+
+        '&:hover $mask': {
+            display: 'flex',
+        },
+
+        '& $mask': {
+            display: 'none',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            width: '100%',
+            height: '100%',
+        }
     }
 }))
 
 const Publish = () => {
     const classes = useStyles()
+    const [files, setFiles] = useState([])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: (acceptedFile) => {
+            const newFiles = acceptedFile.map(file => {
+                return Object.assign(file, {
+                    preview: URL.createObjectURL(file)
+                })
+            })
+
+            setFiles([
+                ...files,
+                ...newFiles,
+            ])
+        }
+    })
 
     return (
         <TemplateDefault>
@@ -85,6 +164,39 @@ const Publish = () => {
                     <Typography component="div" variant="body2" color="textPrimary">
                         A primeira imagem é a foto principal do seu anúncio.
                     </Typography>
+                    <Box className={classes.thumbsContainer}>
+                        <Box className={classes.dropzone} {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <Typography variant="body2" color="textPrimary">
+                                Clique para adicionar ou arraste a imagem para aqui.
+                            </Typography>
+                        </Box>
+
+                        {
+                            files.map((file, index) => (
+                                <Box
+                                key={file.name}
+                                className={classes.thumb}
+                                style={{backgroundImage: `url(${file.preview})`}}
+                                >
+                                    {
+                                        index === 0 ? 
+                                        <Box className={classes.mainImage}>
+                                            <Typography variant="body" color="secondary">
+                                                Principal
+                                            </Typography>
+                                        </Box>
+                                        : null
+                                    }
+                                    <Box className={classes.mask}>
+                                        <IconButton color="secondary">
+                                            <DeleteForever fontSize="large"/>
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            ))
+                        }
+                    </Box>
                 </Box>
             </Container>
 
