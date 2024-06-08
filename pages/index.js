cff9a16dd418618701b5b8486021d10d08c01a73
Guilherme <guiglreis@gmail.com>
2024-06-08 20:46:28 -0300

    Inicio do carrossel

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,13 @@ import {
     InputBase,
     Typography,
     Paper,
-    Card,
-    CardMedia,
-    CardContent, 
     Grid
 } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/search";
 import TemplateDefault from "../src/templates/Default";
+import Card from "@/components/Card";
 
 const useStyles = makeStyles((theme) => ({
     searchBox: {
@@ -21,9 +19,9 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(0, 2),
         marginTop: 20,
     },
-    cardMedia: {
-        paddingTop: '56%',
-    },
+    cardGrid: {
+        marginTop: 50,
+    }
 }))
 
 const Home = () => {
@@ -52,55 +50,25 @@ const Home = () => {
                 <br />
             <Grid container spacing={4}>
                     <Grid item xs={12} sm={6} md={4}>
-                        <Card>
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image={'https://source.unsplash.com/random'}
-                                title="Título da imagem"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="h2">
-                                    Produto X
-                                </Typography>
-                                <Typography>
-                                    R$ 60,00
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                        <Card
+                            image={'https://source.unsplash.com/random'}
+                            title="Produto X"
+                            subtitle="R$ 60,00"
+                        />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4}>
-                        <Card>
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image={'https://source.unsplash.com/random'}
-                                title="Título da imagem"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="h2">
-                                    Produto X
-                                </Typography>
-                                <Typography>
-                                    R$ 60,00
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                        <Card
+                            image={'https://source.unsplash.com/random'}
+                            title="Produto X"
+                            subtitle="R$ 60,00"
+                        />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4}>
-                        <Card>
-                            <CardMedia 
-                                className={classes.cardMedia}
-                                image={'https://source.unsplash.com/random'}
-                                title="Título da imagem"
-                            />
-                            <CardContent>
-                                <Typography variant="h5" component="h2">
-                                    Produto X
-                                </Typography>
-                                <Typography>
-                                    R$ 60,00
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                        <Card
+                            image={'https://source.unsplash.com/random'}
+                            title="Produto X"
+                            subtitle="R$ 60,00"
+                        />
                     </Grid>
                 </Grid>
             </Container>
---
+++ pages/product.js
@@ -0,0 +1,72 @@
+import { Avatar, Box, Card, CardHeader, CardMedia, Chip, Container, Grid, Typography } from "@material-ui/core";
+import TemplateDefault from "../src/templates/Default";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        backgroundColor: theme.palette.background.white,
+        padding: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+    },
+    productName: {
+        margin: '15px 0',
+    },
+    price: {
+        fontWeight: 'bold',
+        marginBottom: 15,
+    }
+}))
+
+const Product = () => {
+    const classes = useStyles()
+
+    return(
+        <TemplateDefault>
+            <Container maxWidth="lg">
+                <Grid container spacing={3}>
+                    <Grid item xs={8}>
+                        <Box className={classes.box}>
+                            Carrossel
+                        </Box>
+
+                        <Box className={classes.box} textAlign="left">
+                            <Typography component="span" variant="caption" className={classes.titles}>Publicado 16 junho de 2024</Typography>
+                            <Typography component="h4" variant="h4" className={classes.productName}>Jaguar XE 2.0 D R-Sport Aut.</Typography>
+                            <Typography component="h4" variant="h4" className={classes.price}>R$ 50.000,00</Typography>
+                            <Chip label="Categoria" />
+                        </Box>
+
+                        <Box className={classes.box} textAlign="left">
+                            <Typography component="h6" variant="h6">Descrição</Typography>
+                            <Typography component="p" variant="body2">
+                                Lorem Ipsum is simply
+                            </Typography>
+                        </Box>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <Card elevation={0} className={classes.box}>
+                            <CardHeader 
+                                avatar={
+                                    <Avatar>G</Avatar>
+                                }
+                                title="Guilherme Reis"
+                                subheader="guiglreis@gmail.com"
+                            />
+                            <CardMedia 
+                                image="https://source.unsplash.com/random"
+                                title="Guilherme Reis"
+                            />
+                        </Card>
+
+                        <Box className={classes.box}>
+                            <Typography component="h6" variant="h6">Localização</Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
+        </TemplateDefault>
+    )
+}
+
+export default Product
